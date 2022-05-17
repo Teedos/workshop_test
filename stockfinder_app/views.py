6dcc48919d366f3001deb62561b71f5cd036e013
Teedos <massimohong2@gmail.com>
2022-05-17 20:25:33 +0200

    testing

--- stockfinder_app/views.py
+++ stockfinder_app/views.py
@@ -8,5 +8,6 @@ def get_home(request):
 
 def search_stock(request):
     searched_stock = request.GET['search']
-    searched_element = sele_test.search_stock('https://www.macrotrends.net/stocks/stock-screener',searched_stock)
-    return render(request,'search_result.html',{'searched_stock':searched_stock, 'searched_element':searched_element})
+    #searched_element = sele_test.search_stock('https://www.macrotrends.net/stocks/stock-screener',searched_stock)
+    #return render(request,'search_result.html',{'searched_stock':searched_stock, 'searched_element':searched_element})
+    return render(request,'search_result.html',{'searched_stock':searched_stock})
