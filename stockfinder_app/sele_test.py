adf80a34fffbc7cba8b903b0394c08fbb2fc3925
Teedos <massimohong2@gmail.com>
2022-05-18 17:07:38 +0200

    bug fixes

--- stockfinder_app/sele_test.py
+++ stockfinder_app/sele_test.py
@@ -6,44 +6,8 @@ import os
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#import requests, bs4
 
-"""def get_history_table(driver,url):
-    #service = Service('/opt/homebrew/bin/geckodriver')
-    #options = Options()
-    #options.headless = True
-    #driver = webdriver.Firefox(service=service,options=options)
-    driver.get(url)
-    table = driver.find_element(By.CLASS_NAME, 'historical_data_table.table')
-    return table.get_attribute('innerHTML')
-    
-def search_name(target, names):
-    for name in names:
-        if target.lower()== name.text.lower():
-            return name.get_attribute('href')
-    return 'Company Not found'
-
-def search_stock(url, target):
-    #service = Service('/opt/homebrew/bin/geckodriver')
-    #options = Options()
-    #options.headless = True
-    #driver = webdriver.Firefox(service=service,options=options) 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get(url)
-    links = driver.find_elements(By.CLASS_NAME, 'ajax-chart a')
-    link = search_name(target, links)
-    table = get_history_table(driver, link)
-    return table"""
-def get_history_table(driver):
-    """"service = Service('/opt/homebrew/bin/geckodriver')
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(service=service,options=options)""" 
+"""def get_history_table(driver):
     #driver.get(url)
     #table = driver.find_element(By.CLASS_NAME, 'historical_data_table.table')
     table = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "historical_data_table.table")))
@@ -57,10 +21,6 @@ def get_description(driver):
 
 
 def search_stock(url, target):
-    """service = Service('/opt/homebrew/bin/geckodriver')
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(service=service,options=options) """
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
@@ -86,13 +46,13 @@ def search_stock(url, target):
         #driver.switch_to.default_content()
         return table, graphic
     #print('nothing found')
-    return False, False
+    return False, False"""
 
 def get_price(driver):
     driver.find_element(By.XPATH,"//span[text() = 'Historical Data']").click()
-    for i in range(0,3):
+    """for i in range(0,3):
         driver.execute_script("window.scrollBy(0,5000)")
-        time.sleep(1)
+        time.sleep(1)"""
     #table = driver.find_element(By.ID,"Col1-1-HistoricalDataTable-Proxy")
     table = driver.find_element(By.CSS_SELECTOR,"table")
     return table.get_attribute('outerHTML')
@@ -121,7 +81,7 @@ def search_finance(url, target):
     #print(price)
     driver.quit()
     return  summary, history
-    
+
 #url = 'https://www.macrotrends.net/stocks/stock-screener'
 #search_bs()
 #print(search_stock(url,'NVIDIA'))
