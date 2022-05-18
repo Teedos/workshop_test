81be3ec4ca4f6fe903abb6dcd1214e30b4399e22
Teedos <massimohong2@gmail.com>
2022-05-18 09:58:06 +0200

    modify selenium

--- stockfinder_app/sele_test.py
+++ stockfinder_app/sele_test.py
@@ -3,15 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time, random
 import os
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #import requests, bs4
 
-def get_history_table(driver,url):
-    """"service = Service('/opt/homebrew/bin/geckodriver')
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(service=service,options=options)""" 
+"""def get_history_table(driver,url):
+    #service = Service('/opt/homebrew/bin/geckodriver')
+    #options = Options()
+    #options.headless = True
+    #driver = webdriver.Firefox(service=service,options=options)
     driver.get(url)
     table = driver.find_element(By.CLASS_NAME, 'historical_data_table.table')
     return table.get_attribute('innerHTML')
@@ -21,6 +22,38 @@ def search_name(target, names):
         if target.lower()== name.text.lower():
             return name.get_attribute('href')
     return 'Company Not found'
+
+def search_stock(url, target):
+    #service = Service('/opt/homebrew/bin/geckodriver')
+    #options = Options()
+    #options.headless = True
+    #driver = webdriver.Firefox(service=service,options=options) 
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.get(url)
+    links = driver.find_elements(By.CLASS_NAME, 'ajax-chart a')
+    link = search_name(target, links)
+    table = get_history_table(driver, link)
+    return table"""
+def get_history_table(driver):
+    """"service = Service('/opt/homebrew/bin/geckodriver')
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(service=service,options=options)""" 
+    #driver.get(url)
+    #table = driver.find_element(By.CLASS_NAME, 'historical_data_table.table')
+    table = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "historical_data_table.table")))
+    return table.get_attribute('innerHTML')
+    
+def get_graphic(driver):
+    driver.switch_to.frame(driver.find_element(By.ID, 'chart_iframe'))
+    graphic = driver.find_element(By.ID, 'chartdiv')
+    return graphic.get_attribute('innerHTML')
+
 
 def search_stock(url, target):
     """service = Service('/opt/homebrew/bin/geckodriver')
@@ -34,30 +67,23 @@ def search_stock(url, target):
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
-    links = driver.find_elements(By.CLASS_NAME, 'ajax-chart a')
-    link = search_name(target, links)
-    table = get_history_table(driver, link)
-    return table
-
-"""def search_name(target, lists):
+    search_bar = driver.find_element(By.NAME, 'q')
+    search_bar.send_keys(target)
+    suggestion_box = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "typeahead__result")))
+    sugg_links = driver.find_elements(By.CLASS_NAME,"typeahead__list a")
+    if sugg_links:
+        #sugg_link = driver.find_element(By.CLASS_NAME,"typeahead__list a")
+        sugg_links[0].click()
+        table = get_history_table(driver)
+        graphic = get_graphic(driver)
+        #print(graphic)
+        #print(table)
+        driver.switch_to.default_content()
+        return table, graphic
+    #print('nothing found')
     return 
+    #print(suggestion_box)
 
-def search_history_table(soup):
-    table = soup.select_one('.historical_data_table.table')
-    return table
-
-def search_bs():
-    url = 'https://www.macrotrends.net/stocks/stock-screener'
-    res = requests.get(url, headers = {
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
-    })
-    target = 'apple'
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
-    print(soup)
-    links = soup.select_one(".ajax-chart")
-    print(links)"""
-    #table = search_history_table
-    #print(table)
 #url = 'https://www.macrotrends.net/stocks/stock-screener'
 #search_bs()
 #print(search_stock(url,'NVIDIA'))
